e67579f85725c8f24f1c6526e346c845b27d211c
Haswitha-Ko <119152181+Haswitha-Ko@users.noreply.github.com>
2024-03-20 12:28:58 +0530

    Create Sol.py

---
+++ Divide and Conquer/Maximum Sum Circular Subarray/Sol.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        def kadane(nums):
+            max_cur=max_global=nums[0]
+            for i in range(1,len(nums)):
+                max_cur=max(nums[i],max_cur+nums[i])
+                max_global=max(max_cur,max_global)
+            return max_global
+        max_linear=kadane(nums)
+        total=sum(nums)
+        max_wrap=total+kadane([-x for x in nums])
+        return max(max_linear,max_wrap) if max_wrap!=0 else max_linear
