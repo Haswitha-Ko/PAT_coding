224a6b76b640dc4f76262968fcb4988106627871
Haswitha-Ko <119152181+Haswitha-Ko@users.noreply.github.com>
2024-03-27 11:57:33 +0530

    Create Sol.py

---
+++ Stacks and Queues/Validate stack sequences/Sol.py
@@ -0,0 +1,11 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        s=[]
+        j=0
+        for x in pushed:
+            s.append(x)
+            while s and s[-1]==popped[j]:
+                s.pop()
+                j+=1
+        return s==[]
+        
